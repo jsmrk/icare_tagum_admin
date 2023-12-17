1c6df8031944ca67270a3afbc9530811696dd834
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2023-12-17 18:28:41 +0800

    concern table UI done

--- lib/manage_concern/manage_concern.dart
+++ lib/manage_concern/manage_concern.dart
@@ -6,9 +6,144 @@ class ManageConcern extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Manage Concern'),
+        body: Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            blurRadius: 5.0,
+            offset: const Offset(1, 1),
+          ),
+        ],
       ),
-    );
+      margin: const EdgeInsets.all(25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          DataTable(
+              border: const TableBorder(
+                horizontalInside: BorderSide(
+                  width: 1,
+                  color: Colors.white,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              headingRowHeight: 45,
+              headingRowColor: const MaterialStatePropertyAll(
+                Color(0xFFF9FAFB),
+              ),
+              headingTextStyle: const TextStyle(
+                fontFamily: 'Inter',
+                color: Colors.grey,
+              ),
+              dataRowMinHeight: 65,
+              dataRowMaxHeight: 65,
+              dataTextStyle: const TextStyle(
+                  fontFamily: 'Inter',
+                  color: Color.fromARGB(255, 75, 75, 75),
+                  fontSize: 13),
+              columns: const [
+                DataColumn(
+                  label: Text('ID'),
+                ),
+                DataColumn(
+                  label: Text('Date'),
+                ),
+                DataColumn(
+                  label: Text('Urgency'),
+                ),
+                DataColumn(
+                  label: Text('Status'),
+                ),
+                DataColumn(
+                  label: Text('Department'),
+                ),
+                DataColumn(
+                  label: Text('Action'),
+                ),
+              ],
+              rows: [
+                DataRow(cells: [
+                  const DataCell(
+                    Text('0'),
+                  ),
+                  DataCell(
+                    Text(DateTime.now().toString()),
+                  ),
+                  const DataCell(
+                    Text('High'),
+                  ),
+                  const DataCell(
+                    Text('Not Viewed'),
+                  ),
+                  const DataCell(
+                    Text('Not Yet Assigned'),
+                  ),
+                  const DataCell(
+                    IconButton(
+                        onPressed: null,
+                        icon: Icon(
+                          Icons.edit,
+                          color: Colors.green,
+                        )),
+                  ),
+                ]),
+                DataRow(cells: [
+                  const DataCell(
+                    Text('0'),
+                  ),
+                  DataCell(
+                    Text(DateTime.now().toString()),
+                  ),
+                  const DataCell(
+                    Text('High'),
+                  ),
+                  const DataCell(
+                    Text('Not Viewed'),
+                  ),
+                  const DataCell(
+                    Text('Not Yet Assigned'),
+                  ),
+                  const DataCell(
+                    IconButton(
+                        onPressed: null,
+                        icon: Icon(
+                          Icons.edit,
+                          color: Colors.green,
+                        )),
+                  ),
+                ]),
+                DataRow(cells: [
+                  const DataCell(
+                    Text('0'),
+                  ),
+                  DataCell(
+                    Text(DateTime.now().toString()),
+                  ),
+                  const DataCell(
+                    Text('High'),
+                  ),
+                  const DataCell(
+                    Text('Not Viewed'),
+                  ),
+                  const DataCell(
+                    Text('Not Yet Assigned'),
+                  ),
+                  const DataCell(
+                    IconButton(
+                        onPressed: null,
+                        icon: Icon(
+                          Icons.edit,
+                          color: Colors.green,
+                        )),
+                  ),
+                ]),
+              ]),
+        ],
+      ),
+    ));
   }
 }
